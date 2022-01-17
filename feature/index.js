d7dd55d3f90cdfcfad54c90dea67a07ccbc61757
thunk <andy.brownlie@digital54.co>
2022-01-17 16:52:51 +0000

    interm checkin on template files

--- feature/index.js
+++ feature/index.js
@@ -29,42 +29,66 @@ module.exports = class extends Generator {
 
     this.props = extend(this.props, input);
   }
+
+ get writing() {
+  return {
+    appStaticFiles() {
+      const src = this.sourceRoot() + '/**';
+      const dest = this.destinationPath(this.props.featureName);
+
+      const files = [
+        'index.ts',
+      ];
+
+      const copyOpts = {
+        globOptions: {
+          ignore: [],
+        },
+      };
 /*
-  get writing() {
-    return {
-      appStaticFiles() {
-        const src = this.sourceRoot() + '/**';
-        const dest = this.destinationPath(this.featureName);
-
-        const files = [
-          'index.ts',
-        ];
-
-        const copyOpts = {
-          globOptions: {
-            ignore: [],
-          },
-        };
-
-        this.fs.copy(src, dest, copyOpts);
-        this.fs.copy(this.templatePath('.*'), dest, copyOpts);
-
-        const opts = {
-          name: this.featureName
-        };
-
-        files.forEach(f => {
-          this.fs.copyTpl(
-            this.templatePath(f),
-            this.destinationPath(`${this.featureName}/${f}`),
-            opts,
-            copyOpts
-          );
-        });
+      if (this.specification === 'openapi_3') {
+        copyOpts.globOptions.ignore.push(src + '/server/common/swagger.js');
+        copyOpts.globOptions.ignore.push(src + '/server/common/api.v2.yml');
+      } else {
+        files.push('server/common/api.v2.yml');
+        copyOpts.globOptions.ignore.push(src + '/server/common/api.yml');
       }
-    }
-  }
+      if (!this.docker) {
+        copyOpts.globOptions.ignore.push(
+          src + '/+(Dockerfile|.dockerignore)'
+        );
+      }
 */
+      this.fs.copy(src, dest, copyOpts);
+      //this.fs.copy(this.templatePath('.*'), dest, copyOpts);
+
+      const opts = {
+        featureName: this.props.featureName
+      };
+
+      files.forEach(f => {
+        this.fs.copyTpl(
+          this.templatePath(f),
+          this.destinationPath(`${this.props.featureName}/${f}`),
+          opts,
+          copyOpts
+        );
+      });
+/*
+      this.fs.move(
+        this.destinationPath(`${this.name}`, 'gitignore'),
+        this.destinationPath(`${this.name}`, '.gitignore')
+      );
+      if (this.specification !== 'openapi_3') {
+        this.fs.move(
+          this.destinationPath(`${this.name}`, 'server/common/api.v2.yml'),
+          this.destinationPath(`${this.name}`, 'server/common/api.yml')
+        );
+      }
+*/
+    },
+  };
+}
   default() {
     console.log(this.props);
     
